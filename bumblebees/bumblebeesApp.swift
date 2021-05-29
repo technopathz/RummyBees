add12447063be115ffa246b6cd15d8dfb42715ab
Kongara, Bharath <kbharath00@gmail.com>
2021-05-29 11:05:34 -0500

    Initial Commit

---
+++ bumblebees/bumblebeesApp.swift
@@ -0,0 +1,17 @@
+//
+//  bumblebeesApp.swift
+//  bumblebees
+//
+//  Created by Kongara, Bharath on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct bumblebeesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
